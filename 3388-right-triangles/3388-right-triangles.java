28e9ab366ba7dea0b101e56fd6ad35952073fd83
HemaDua1 <162971639+HemaDua1@users.noreply.github.com>
2024-08-25 17:07:16 +0530

    Time: 11 ms (93.51%), Space: 146.7 MB (20.78%) - LeetHub

---
+++ 3388-right-triangles/3388-right-triangles.java
@@ -0,0 +1,26 @@
+class Solution {
+    public long numberOfRightTriangles(int[][] grid) {
+        int[] row = new int[grid.length];
+        Arrays.fill(row, 0);
+        
+        int[] col = new int[grid[0].length];
+        Arrays.fill(col, 0);
+        
+        for(int i=0; i<grid.length; i++){
+            for(int j=0; j<grid[0].length; j++){
+                row[i]+=grid[i][j];
+                col[j]+=grid[i][j];
+            }
+        }
+        
+        long res=0;
+        for(int i=0; i<grid.length; i++){
+            for(int j=0; j<grid[0].length; j++){
+                if(grid[i][j]==1){
+                    res+=(row[i]-1)*(col[j]-1);
+                }
+            }
+        }
+        return res;
+    }
+}
