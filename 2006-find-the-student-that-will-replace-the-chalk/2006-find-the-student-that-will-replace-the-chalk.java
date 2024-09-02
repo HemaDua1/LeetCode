6e9565acb2f1731b3ea524ef1f04353b265863cb
HemaDua1 <162971639+HemaDua1@users.noreply.github.com>
2024-09-02 22:28:04 +0530

    Time: 2 ms (72.59%), Space: 59.4 MB (87.17%) - LeetHub

---
+++ 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int chalkReplacer(int[] chalk, int k) {
+    long total=0;
+    for(int i:chalk)
+    total+=i;
+    k%=total;
+      for (int i = 0; i < chalk.length; i++) {
+            if (k < chalk[i]) {
+                return i;
+            }
+            k -= chalk[i];
+        }
+        return 0;
+    }
+}
