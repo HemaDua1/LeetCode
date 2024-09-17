6764d0500d6994336b0d28a04409f7e01a0e988f
HemaDua1 <162971639+HemaDua1@users.noreply.github.com>
2024-09-17 15:26:40 +0530

    Time: 4 ms (74.12%), Space: 44.2 MB (68.55%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,29 +10,53 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists.length == 0){
-            return null;
-        }
-        PriorityQueue<ListNode> minHeap = new PriorityQueue<>((a, b) -> a.val - b.val);
-        for(ListNode head : lists){
-            if(head != null)
-                minHeap.add(head);
-        }
+    //     if(lists.length == 0){
+    //         return null;
+    //     }
+    //     PriorityQueue<ListNode> minHeap = new PriorityQueue<>((a, b) -> a.val - b.val);
+    //     for(ListNode head : lists){
+    //         if(head != null)
+    //             minHeap.add(head);
+    //     }
 
-        ListNode dummyHead = new ListNode(-1);
-        ListNode ll = dummyHead;
+    //     ListNode dummyHead = new ListNode(-1);
+    //     ListNode ll = dummyHead;
         
-        while(!minHeap.isEmpty()){
-            ListNode curr = minHeap.remove();
-            ll.next = curr;
+    //     while(!minHeap.isEmpty()){
+    //         ListNode curr = minHeap.remove();
+    //         ll.next = curr;
 
-            ll = ll.next;
-            if(curr.next != null){
-                minHeap.add(curr.next);
-            }
+    //         ll = ll.next;
+    //         if(curr.next != null){
+    //             minHeap.add(curr.next);
+    //         }
+    //     }
+    //     ListNode newHead = dummyHead.next;
+    //     dummyHead.next = null;
+    //     return newHead ;
+    // }
+    if(lists.length==0)
+    return null;
+    // PriorityQueue<ListNode> minheap=new PriorityQueue<>((a,b)-> a.val-b.val);
+    PriorityQueue<ListNode> minheap=new PriorityQueue<ListNode>(
+        new Comparator<ListNode>(){
+            public int compare(ListNode a,ListNode b){
+            return a.val-b.val;
         }
-        ListNode newHead = dummyHead.next;
-        dummyHead.next = null;
-        return newHead ;
+        });
+    for(ListNode head:lists){
+        if(head!=null)
+        minheap.add(head);
     }
+   ListNode dummyhead=new ListNode(-1); 
+   ListNode temp=dummyhead;
+while(!minheap.isEmpty()){
+    ListNode m=minheap.poll();
+    dummyhead.next=m;
+    dummyhead=dummyhead.next;
+    if(m.next!=null)
+    minheap.add(m.next);
+}
+return temp.next;
+}
 }
