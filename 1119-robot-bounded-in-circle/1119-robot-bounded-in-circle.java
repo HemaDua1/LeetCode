32955d8c71491cbc8289ac4e0aeacadaa31df81c
HemaDua1 <162971639+HemaDua1@users.noreply.github.com>
2024-09-20 11:48:04 +0530

    Time: 0 ms (100%), Space: 41.2 MB (67.83%) - LeetHub

--- 1119-robot-bounded-in-circle/1119-robot-bounded-in-circle.java
+++ 1119-robot-bounded-in-circle/1119-robot-bounded-in-circle.java
@@ -1,14 +1,17 @@
 class Solution {
-        public boolean isRobotBounded(String ins) {
-        int x = 0, y = 0, i = 0, d[][] = {{0, 1}, {1, 0}, {0, -1}, { -1, 0}};
-        for (int j = 0; j < ins.length(); ++j)
-            if (ins.charAt(j) == 'R')
-                i = (i + 1) % 4;
-            else if (ins.charAt(j) == 'L')
-                i = (i + 3) % 4;
-            else {
-                x += d[i][0]; y += d[i][1];
-            }
-        return x == 0 && y == 0 || i > 0;
+    public boolean isRobotBounded(String inst) {
+    int x=0,y=0,j=0;
+    int d[][]={{0,1},{1,0},{0,-1},{-1,0}};
+    for(int i=0;i<inst.length();i++){
+if(inst.charAt(i)=='R')
+j=(j+1)%4;
+else if(inst.charAt(i)=='L')
+j=(j+3)%4;
+else{
+    x+=d[j][0];
+    y+=d[j][1];
+}
+    } 
+     return x==0 && y==0 || j>0;  
     }
 }
