d01398f099268a9b41a086177891b08525d10a01
HemaDua1 <162971639+HemaDua1@users.noreply.github.com>
2024-09-24 15:29:20 +0530

    Time: 52 ms (32.23%), Space: 45.4 MB (25.85%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,16 +1,27 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> l=new ArrayList<>();
-        char gh[]=p.toCharArray();
-        Arrays.sort(gh);
-p=new String(gh);
-    for(int i=0;i<=s.length()-p.length();i++){
-        char ch[]=(s.substring(i,i+p.length())).toCharArray();
-        Arrays.sort(ch);
-        String k=new String(ch);
-if(k.equals(p))
-l.add(i);
-    }  
-    return l;  
+        if(s.length()<p.length())
+        return l;
+        HashMap<Character,Integer> h1=new HashMap<>();
+        HashMap<Character,Integer> h2=new HashMap<>();
+        for(char ch:p.toCharArray())
+        h1.put(ch,h1.getOrDefault(ch,0)+1);
+        for(char gh:s.substring(0,p.length()).toCharArray())
+        h2.put(gh,h2.getOrDefault(gh,0)+1);
+        if(h1.equals(h2))
+        l.add(0);
+        for(int i=p.length();i<s.length();i++){
+            char o=s.charAt(i-p.length());
+            if(h2.get(o)==1)
+            h2.remove(o);
+            else
+            h2.put(o,h2.get(o)-1);
+            char n=s.charAt(i);
+            h2.put(n,h2.getOrDefault(n,0)+1);
+            if(h1.equals(h2))
+            l.add(i-p.length()+1);
+        }
+return l;
     }
 }
