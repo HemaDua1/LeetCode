58a6f71e7c65c66b18b4c6545d4fdc5119f58d37
HemaDua1 <162971639+HemaDua1@users.noreply.github.com>
2024-09-24 14:39:26 +0530

    Time: 35 ms (9.07%), Space: 49.6 MB (5.07%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -2,14 +2,26 @@ class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         HashMap<String,List<String>> h=new HashMap<>();
         for(String s:strs){
-            char c[]=s.toCharArray();
-            Arrays.sort(c);
-            String sor=new String(c);
+            // char c[]=s.toCharArray();
+            // Arrays.sort(c);
+            // String sor=new String(c);
+            String sor=findKey(s);
             if(!h.containsKey(sor))
             h.put(sor,new ArrayList<>());
             h.get(sor).add(s);
         }
         List<List<String>> l=new ArrayList<>(h.values());
         return l;
+    }
+    public String findKey(String s){
+        int[] arr=new int[26];
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            arr[ch-'a']++;
+        }
+        String k="";
+        for(int a:arr)
+        k=k+"#"+a;
+        return k;
     }
 }
