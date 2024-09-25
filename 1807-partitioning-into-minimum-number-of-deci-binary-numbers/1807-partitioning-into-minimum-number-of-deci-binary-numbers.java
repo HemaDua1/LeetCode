f9685714dc2f6a67d3f4c2cfba8228fc50a0d674
HemaDua1 <162971639+HemaDua1@users.noreply.github.com>
2024-09-25 12:41:16 +0530

    Time: 6 ms (52.5%), Space: 44.9 MB (80.02%) - LeetHub

--- 1807-partitioning-into-minimum-number-of-deci-binary-numbers/1807-partitioning-into-minimum-number-of-deci-binary-numbers.java
+++ 1807-partitioning-into-minimum-number-of-deci-binary-numbers/1807-partitioning-into-minimum-number-of-deci-binary-numbers.java
@@ -2,16 +2,9 @@ class Solution {
     public int minPartitions(String n) {
     int ans=0;
      for(int i=0;i<n.length();i++){
-        int a=check(n.charAt(i)-'0');
+        int a=n.charAt(i)-'0';
         ans=Math.max(a,ans);
      }   
-     return ans-1;
-    }
-    public int check(int c){
-int a=1;
-while(a<=c){
-    a=a+1;
-}
-return a;
+     return ans;
     }
 }
