a02202f886c4d5e6eb70b3134443f65f9d036213
HemaDua1 <162971639+HemaDua1@users.noreply.github.com>
2024-09-25 12:10:32 +0530

    Time: 41 ms (42.81%), Space: 65.7 MB (55.49%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,20 +1,31 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        if(nums.length==0){
-            return 0;
-        }
-        Arrays.sort(nums);
-        int c = 1;
-        int s = 1;
-        for (int i = 1; i < nums.length; i++) {
-        if (nums[i] == nums[i - 1] + 1) {
-                s++;
-                c = Math.max(c,s);
-            } else if (nums[i] != nums[i - 1]) {
-                s = 1;
-            }
-        }
-        
-        return c;
+        if(nums.length==0)
+        return 0;
+      HashMap<Integer,Boolean> h=new HashMap<>();
+      for(int i:nums)
+      h.put(i,true);
+    //   for(int i=0;i<nums.length;i++){
+    //     if(h.containsKey(nums[i]+1))
+    //     h.put(nums[i]+1,false);
+    //     else if(h.containsKey(nums[i]-1))
+    //     h.put(nums[i],false);
+    //     else
+    // h.put(nums[i],true);
+    //   } 
+    for(int i:nums){
+        if(h.containsKey(i-1))
+        h.put(i,false);
+    }
+      int ans=0; 
+      for(int i:h.keySet()){
+        int k=1;
+       if(h.get(i)==true){
+       while(h.containsKey(i+k))
+       k++;
+       }
+       ans=Math.max(ans,k);
+      }
+      return ans;
     }
 }
