ec6e25c03256b939aa539ab84e4194d8e71f1e23
HemaDua1 <162971639+HemaDua1@users.noreply.github.com>
2024-10-09 21:24:49 +0530

    Time: 0 ms (100%), Space: 41.3 MB (79.36%) - LeetHub

---
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+  int o =0, m=0;
+    for(int i=0; i<s.length(); i++)
+    {
+     if(s.charAt(i)=='(')
+     o++;
+     else
+     {
+        if(o>0)
+        o--;
+        else
+        m++;
+     }
+    } 
+     return o+m;   
+    }
+}
