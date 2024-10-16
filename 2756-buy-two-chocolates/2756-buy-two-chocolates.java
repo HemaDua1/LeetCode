4f1496b9283a1e68bde4b6239b5ac77ab1a3c6c9
HemaDua1 <162971639+HemaDua1@users.noreply.github.com>
2024-10-16 18:43:15 +0530

    Time: 1 ms (100%), Space: 44.3 MB (58.31%) - LeetHub

--- 2756-buy-two-chocolates/2756-buy-two-chocolates.java
+++ 2756-buy-two-chocolates/2756-buy-two-chocolates.java
@@ -1,7 +1,22 @@
 class Solution {
-    public int buyChoco(int[] prices, int money) {
-    Arrays.sort(prices);
-    int s=prices[0]+prices[1];
+    public int buyChoco(int[] arr, int money) {
+    // Arrays.sort(prices);
+    // int s=prices[0]+prices[1];
+    // if(money-s>-1)
+    // return money-s; 
+    // else
+    // return money;   
+    int min=Integer.MAX_VALUE;
+    int smin=Integer.MAX_VALUE;
+    for(int i=0;i<arr.length;i++){
+        if(arr[i]<min){
+            smin=min;
+            min=arr[i];
+        }
+        else if(arr[i]<smin)
+        smin=arr[i];
+    }
+    int s=min+smin;
     if(money-s>-1)
     return money-s; 
     else
