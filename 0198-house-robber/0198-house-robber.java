c80b9e3c03aa4153c4ca24231a9d50ac734840be
HemaDua1 <162971639+HemaDua1@users.noreply.github.com>
2024-11-18 15:33:26 +0530

    Time: 0 ms (100%), Space: 41.4 MB (5.12%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -9,9 +9,9 @@ if(l==2)
 return Math.max(n[0],n[1]);
 int dp[]=new int[l];
 dp[0]=n[0];
-dp[1]=Integer.max(n[0],n[1]);
+dp[1]=Math.max(n[0],n[1]);
 for(int i=2;i<l;i++)
-dp[i]=Integer.max(n[i]+dp[i-2],dp[i-1]);
+dp[i]=Math.max(n[i]+dp[i-2],dp[i-1]);
 return dp[l-1];
 
     }
