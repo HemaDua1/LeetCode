ee6e26d02e9f5c02cd5c544d3c496ad6d0b8fcea
Hema dua <162971639+HemaDua1@users.noreply.github.com>
2024-11-21 14:33:30 +0530

    Time: 0 ms (100%), Space: 41.1 MB (37.72%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -5,18 +5,26 @@ if(l==0)
 return 0;
 int[] dp=new int[l];
 Arrays.fill(dp,-1);
-return ans(l-1,dp,n);
+return ans(0,dp,n);
     }
     public static int ans(int i,int[] dp,int[] nums){
-        if(i<0)
+        // if(i<0)
+        // return 0;
+        // if(i==0)
+        // return nums[i];
+        // if(dp[i]!=-1)
+        // return dp[i];
+        // int a=ans(i-1,dp,nums);
+        // int b=ans(i-2,dp,nums)+nums[i];
+        // dp[i]=Math.max(a,b);
+        // return dp[i];
+       if (i>=nums.length)
         return 0;
-        if(i==0)
-        return nums[i];
         if(dp[i]!=-1)
         return dp[i];
-        int a=ans(i-1,dp,nums);
-        int b=ans(i-2,dp,nums)+nums[i];
-        dp[i]=Math.max(a,b);
+        int rob=nums[i]+ans(i+2,dp,nums);
+        int drob=ans(i+1,dp,nums);
+        dp[i]=Math.max(rob,drob);
         return dp[i];
     }
 }
