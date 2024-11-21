5289ce1ddf4c80f7553bec376bcb88673fd1180d
Hema dua <162971639+HemaDua1@users.noreply.github.com>
2024-11-21 11:55:17 +0530

    Time: 0 ms (100%), Space: 40.9 MB (64.33%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,18 +1,22 @@
 class Solution {
     public int rob(int[] n) {
 int l=n.length;
-if(n==null||l==0)
+if(l==0)
 return 0;
-if(l==1)
-return n[0];
-if(l==2)
-return Math.max(n[0],n[1]);
-int dp[]=new int[l];
-dp[0]=n[0];
-dp[1]=Math.max(n[0],n[1]);
-for(int i=2;i<l;i++)
-dp[i]=Math.max(n[i]+dp[i-2],dp[i-1]);
-return dp[l-1];
-
+int[] dp=new int[l];
+Arrays.fill(dp,-1);
+return ans(l-1,dp,n);
+    }
+    public static int ans(int i,int[] dp,int[] nums){
+        if(i<0)
+        return 0;
+        if(i==0)
+        return nums[i];
+        if(dp[i]!=-1)
+        return dp[i];
+        int a=ans(i-1,dp,nums);
+        int b=ans(i-2,dp,nums)+nums[i];
+        dp[i]=Math.max(a,b);
+        return dp[i];
     }
 }
