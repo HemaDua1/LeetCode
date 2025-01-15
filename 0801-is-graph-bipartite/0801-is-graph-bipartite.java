5efcea2031b58229c962854d7063253b8358f6cd
Hema dua <162971639+HemaDua1@users.noreply.github.com>
2025-01-15 11:43:08 +0530

    Time: 8 ms (6.41%), Space: 45.1 MB (48.3%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.java
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.java
@@ -1,23 +1,41 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
-      int arr[]=new int[graph.length];
-  Arrays.fill(arr,-1);
-  for(int i=0;i<graph.length;i++){
-    Queue<Integer> q=new LinkedList<>();
-    q.add(i);
-    arr[0]=0;
-    while(!q.isEmpty()){
-        int curr=q.poll();
-        for(int nbr:graph[curr]){
-            if(arr[nbr]==-1){
-                arr[nbr]=1-arr[curr];
-                q.add(nbr);
+        Queue<BiparttitePair> q = new LinkedList<>();
+        HashMap<Integer, Integer> visited = new HashMap<>();
+        int n=graph.length;
+        for(int vtx=0;vtx<n;vtx++){
+            //BFS
+            if(visited.containsKey(vtx)){
+                continue;
             }
-            else if(arr[nbr]==arr[curr])
-            return false;
+            q.add(new BiparttitePair(vtx, 0));
+            while(!q.isEmpty()){
+                //1. Remove
+                BiparttitePair rp = q.poll();
+                //2. Ignore if already visited
+                if(visited.containsKey(rp.vtx)){
+                    if(visited.get(rp.vtx)!=rp.dis)//odd length ka cycle
+                    return false;
+                    continue;
+                }
+                //3. Mark visited
+                visited.put(rp.vtx, rp.dis);
+                for(int nbrs:graph[rp.vtx]){
+                    if(!visited.containsKey(nbrs)){
+                        q.add(new BiparttitePair(nbrs, rp.dis+1));
+                    }
+                }
+                //5. Add unvisited nbrs
+            }
         }
+        return true;
     }
-  }
-  return true;
+    class BiparttitePair{
+        int vtx;
+        int dis;
+        BiparttitePair(int vtx, int dis){
+            this.vtx = vtx;
+            this.dis = dis;
+        }
     }
 }
