9498d35e20b400bd466b7cb0c0a69ce06241d08c
Hema dua <162971639+HemaDua1@users.noreply.github.com>
2025-02-06 11:06:13 +0530

    Time: 1 ms (100%), Space: 45.9 MB (44.09%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,17 +1,17 @@
 class Solution {
-    public int[] singleNumber(int[] nums) {
-    int arr[]=new int[2];
-    HashMap<Integer,Integer> h= new HashMap<>();
-    for(int i:nums)
-    h.put(i,h.getOrDefault(i,0)+1);
-    int j=0;
-    for(int i:h.keySet()){
-        if(h.get(i)==1){
-            arr[j]=i;
-            j++;
+    public int[] singleNumber(int[] arr) {
+        int ans=0;
+        for(int i=0;i<arr.length;i++){
+            ans^=arr[i];
         }
-
-    } 
-    return arr;   
+        int  mask=(ans & (~(ans-1)));
+        //mask=ans & -ans;
+        int a=0;
+        for(int i=0;i<arr.length;i++){
+            if((arr[i]&mask)!=0)
+            a^=arr[i];
+        }
+        int b=ans^a;
+        return new int[]{a,b};
     }
 }
