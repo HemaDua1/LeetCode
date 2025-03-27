3fb41506cf401370ca38ef26dc813a95a44ba251
Hema dua <162971639+HemaDua1@users.noreply.github.com>
2025-03-27 11:28:49 +0530

    Time: 11 ms (66.71%), Space: 45.5 MB (59.8%) - LeetHub

---
+++ 1508-longest-happy-prefix/1508-longest-happy-prefix.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String longestPrefix(String s) {
+        return LPS(s);
+
+    }
+    public static String LPS(String s){
+        int[] dp=new int[s.length()];
+        int len=0;
+        int i=1;
+        while(i<s.length()){
+            if(s.charAt(i)==s.charAt(len)){
+                dp[i]=++len;
+                i++;
+            }
+            else{
+                if(len>0){
+                    len=dp[len-1];
+                }
+                else{
+                    dp[i]=0;
+                    i++;
+                }
+            }
+        }
+        return s.substring(0,dp[dp.length-1]);
+    }
+}
