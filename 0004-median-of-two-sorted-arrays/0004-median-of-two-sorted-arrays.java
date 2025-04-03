5faa863159d62ddeb318415afe4fba79c4ca2b34
Hema dua <162971639+HemaDua1@users.noreply.github.com>
2025-04-03 12:32:59 +0530

    Time: 1 ms (100%), Space: 46.4 MB (26%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,23 +1,35 @@
-import java.util.Arrays;
-
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
- int n=nums1.length;
- int m=nums2.length;
- int mer[]=new int[n+m];
- int k=0;
- for(int i=0;i<n;i++)
- mer[k++]=nums1[i];
-  for(int i=0;i<m;i++)
- mer[k++]=nums2[i];
- Arrays.sort(mer);
- int t=mer.length;
-if(t%2!=0)
-return (double)mer[t/2];
-else{
-    int i=mer[t/2-1];
-    int j=mer[t/2];
-    return (double)(i+j)/2.00;
-}
+     if(nums1.length<=nums2.length)
+     return median(nums1,nums2);
+     else
+     return median(nums2,nums1);
+    }
+    public static double median(int[] nums1,int[] nums2){
+        int lo=0;
+     int hi=nums1.length;
+     int n=nums1.length,m=nums2.length;
+     while(lo<=hi){
+        int cut1=(lo+hi)/2;
+        int cut2=((n+m+1)/2)-cut1;
+        int l1=cut1==0?Integer.MIN_VALUE:nums1[cut1-1];
+        int r1=cut1==n?Integer.MAX_VALUE:nums1[cut1];
+        int l2=cut2==0?Integer.MIN_VALUE:nums2[cut2-1];
+        int r2=cut2==m?Integer.MAX_VALUE:nums2[cut2];
+        if(l1<=r2 && l2<=r1){
+            if((n+m)%2==0)
+            return (Math.max(l1,l2)+Math.min(r1,r2))/2.0;
+            else
+            return Math.max(l1,l2);
+        }
+        else if(l1>=r2){
+            hi=cut1-1;
+        }
+        else{
+            lo=cut1+1;
+        }
+
+     }
+     return 0.0;
     }
 }
