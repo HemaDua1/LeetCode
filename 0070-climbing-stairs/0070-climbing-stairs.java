838d86373ea3155c6c6a4686283f276ffffc6295
Hema dua <162971639+HemaDua1@users.noreply.github.com>
2025-08-10 20:25:47 +0530

    Time: 0 ms (100%), Space: 40.8 MB (10.4%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,10 +1,13 @@
 class Solution {
     public int climbStairs(int n) {
-    int[] dp=new int[n+1];
-    dp[0]=1;
-    dp[1]=1;
-    for(int i=2;i<=n;i++)
-    dp[i]=dp[i-1]+dp[i-2];
-    return dp[n];
+   int prev2=1,prev1=1,curr=0;
+   for(int i=2;i<=n;i++){
+    curr=prev1+prev2;
+    prev2=prev1;
+    prev1=curr;
+   }
+   if(n==0||n==1)
+   return 1;
+   return curr;
     }
 }
